cd04fa6931ff326f2d8d45dc99cfdb78f15d77a7
mwyaa7 <130219681+mwyaa7@users.noreply.github.com>
2023-07-25 22:48:21 +0900

    commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,9 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :email, presence: true
-  validates :encrypted_password, presence: true
-  validates :name_chinese_characters, presence: true
-  validates :first_name_chinese_characters, presence: true
-  validates :name_katakana, presence: true
+  validates_format_of :encrypted_password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  validates :name_chinese_characters, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :first_name_chinese_characters, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+  validates :name_katakana, presence: true, format: { with: /\A[ァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
   validates :date_of_birth, presence: true
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,10 @@ FactoryBot.define do
     nickname              {'test'}
     email                 {'test@example'}
     password              {'000000'}
-    password_confirmation {password}
+    name_chinese_characters{'test'}
+    first_name_chinese_characters{'test'}
+    name_katakana{'test'}
+    first_name_katakana{'test'}
+    date_of_birth{'000000'}
   end
 end
