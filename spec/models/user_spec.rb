e10927e6c69f251bcec7a559e62aa0de0a9a89b0
mwyaa7 <130219681+mwyaa7@users.noreply.github.com>
2023-07-26 21:15:05 +0900

    commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,39 @@ require 'rails_helper'
       @user.valid?
        expect(@user.errors.full_messages).to include("Email can't be blank")
 
-     end
+       end
+      it "passwordが空では登録できない" do
+        @user.password = ''
+      @user.valid?
+    expect(@user.errors.full_messages).to include("Password can't be blank")
+
    end
+   it "name_chinese_charactersが空では登録できない" do
+    @user.name_chinese_characters = ''
+      @user.valid?
+    expect(@user.errors.full_messages).to include("name_chinese_characters can't be blank")
  end
+  
+ it "first_name_chinese_charactersが空では登録できない" do
+  @user.first_name_chinese_characters = ''
+      @user.valid?
+  expect(@user.errors.full_messages).to include("first_name_chinese_characters can't be blank")
+end
+
+it "name_katakanaが空では登録できない" do
+  @user.name_katakana = ''
+      @user.valid?
+  expect(@user.errors.full_messages).to include("name_katakana can't be blank")
+end
+
+it "first_name_katakanaが空では登録できない" do
+  @user.first_name_katakana = ''
+      @user.valid?
+  expect(@user.errors.full_messages).to include("first_name_katakana can't be blank")
+end
+
+it "date_of_birthが空では登録できない" do
+  @user.date_of_birth = ''
+      @user.valid?
+  expect(@user.errors.full_messages).to include("date_of_birth can't be blank")
+end
