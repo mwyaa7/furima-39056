48f636ae99db1acf7f14090b0b0353c8fbf925c8
mwyaa7 <130219681+mwyaa7@users.noreply.github.com>
2023-07-27 12:43:19 +0900

    commit

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -51,13 +51,33 @@ require 'rails_helper'
       expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
+    it '英字のみのパスワードでは登録できない' do
+      @user.password = ''
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be only English")
+    end
+
+    it '数字のみのパスワードでは登録できない' do
+      @user.password = ''
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be only number")
+    end
+
+    it '全角文字を含むパスワードでは登録できない' do
+      @user.password = ''
+      @user.password_confirmation = ''
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Password can't be full_width ")
+    end
+
     it 'passwordとpassword_confirmationが不一致では登録できない' do
       @user.password = '123456'
       @user.password_confirmation = '1234567'
       @user.valid?
       expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-
   
    it "name_chinese_charactersが空では登録できない" do
     @user.name_chinese_characters = ''
@@ -65,10 +85,24 @@ require 'rails_helper'
     expect(@user.errors.full_messages).to include("name_chinese_characters can't be blank")
  end
   
+ it "姓（全角）に半角文字が含まれていると登録できない" do
+  @user.name_chinese_characters = ''
+    @user.valid?
+  expect(@user.errors.full_messages).to include("name_chinese_characters can't be half_width")
+end
+
+
+
  it "first_name_chinese_charactersが空では登録できない" do
   @user.first_name_chinese_characters = ''
       @user.valid?
   expect(@user.errors.full_messages).to include("first_name_chinese_characters can't be blank")
+end
+
+it "名（全角）に半角文字が含まれていると登録できない" do
+  @user.name_chinese_characters = ''
+    @user.valid?
+  expect(@user.errors.full_messages).to include("first_name_chinese_characters can't be half_width")
 end
 
 it "name_katakanaが空では登録できない" do
@@ -77,10 +111,22 @@ it "name_katakanaが空では登録できない" do
   expect(@user.errors.full_messages).to include("name_katakana can't be blank")
 end
 
+it "姓（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない" do
+  @user.name_katakana = ''
+      @user.valid?
+  expect(@user.errors.full_messages).to include("name_katakana can't be only Katakana")
+end
+
 it "first_name_katakanaが空では登録できない" do
   @user.first_name_katakana = ''
       @user.valid?
   expect(@user.errors.full_messages).to include("first_name_katakana can't be blank")
+end
+
+it "名（カナ）にカタカナ以外の文字（平仮名・漢字・英数字・記号）が含まれていると登録できない" do
+  @user.first_name_katakana = ''
+      @user.valid?
+  expect(@user.errors.full_messages).to include("first_name_katakana can't be only Katakana")
 end
 
 it "date_of_birthが空では登録できない" do
